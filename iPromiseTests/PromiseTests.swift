2adb911969af4d043d399640c3584ce1deb3b00e
jzaczek <zaczekjakub@gmail.com>
2015-11-20 13:51:31 +0100

    Added travis.yml + removed unwanted methods from Promise tests

--- iPromiseTests/PromiseTests.swift
+++ iPromiseTests/PromiseTests.swift
@@ -15,16 +15,6 @@ class PromiseTests: XCTestCase {
         case Error
     }
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     /**
     Tests promise fulfilling mechanisms
     */
