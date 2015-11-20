62f33f2d3eeb8963880a8e8ec364d529f8ec0250
jzaczek <zaczekjakub@gmail.com>
2015-11-20 19:38:21 +0100

    Created Cocoa Pods workspace

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'iPromise' do
+
+end
+
+target 'iPromiseTests' do
+
+end
+
---
+++ iPromise/iPromise.h
@@ -0,0 +1,19 @@
+//
+//  iPromise.h
+//  iPromise
+//
+//  Created by jzaczek on 20.11.2015.
+//  Copyright © 2015 jzaczek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iPromise.
+FOUNDATION_EXPORT double iPromiseVersionNumber;
+
+//! Project version string for iPromise.
+FOUNDATION_EXPORT const unsigned char iPromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iPromise/PublicHeader.h>
+
+
